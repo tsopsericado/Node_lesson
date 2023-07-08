47b4b8e5f838c7bba14bcbfebbe5a3291d6b5264
tsopsericado <ricardotsopse@gmail.com>
2023-07-08 12:11:40 +0100

    another file created and some functionalities added

--- student-ms/server.js
+++ student-ms/server.js
@@ -1,5 +1,5 @@
-let express = require('express')
-const bodyParser =require('body-parser')
+let express = require("express");
+const bodyParser = require("body-parser");
 const students = [
   {
     Id: "1",
@@ -27,63 +27,76 @@ const students = [
   },
 ];
 
-let app = express()
+let app = express();
 
 // app.use(bodyParser.json());
 
 // getting the array of students
-const StudentsGroup = (req, res)=>{
-res.json( students );
-}
+const StudentsGroup = (req, res) => {
+  res.json(students);
+};
 
 // getting a student id
 const studentId = (req, res) => {
-    const stdtid = req.params.id
-    res.send(`Object ID:${stdtid}`);
+  const stdtid = req.params.id;
+  res.send(`Object ID:${stdtid}`);
 };
 
 //creating a new student
-const createStudent = (req, res)=>{
-    // const { name, age, level } = req.body
-    // console.log(req.body)
-    // console.log( name, age, level)
-    // res.json({ name, age, level})
-    students = req.body;
-    res.json({message: "JSON object created successfully"});
-}
+const createStudent = (req, res) => {
+  students = req.body;
+  res.json({ message: "JSON object created successfully" });
+};
 
 //deleting a student from the list
-const deleteStudents = (req, res)=>{
-   students = students.filter(object =>{
-    return object.Id !=req.params.id
+const deleteStudents = (req, res) => {
+  students = students.filter((object) => {
+    return object.Id != req.params.id;
   });
-  res.json(students)
-}
+  res.json(students || "JSON object deleted succesfully") ||
+    res.status(404).json({ error: "JSON object  not found" });
+};
 
-const updateStudents = (req, res)=>{
- //update the JSON object with new values from the request body
- students.name = req.body.name;
- students.age = req.body.age;
- students.level = req.body.level;
- 
-res.send('JSON updated succesfully')
-}
+//update the JSON object with new values from the request body
+const updateStudents = (req, res) => {
+  // console.log("BODY", req.body);
+  const update = req.body;
+  id = req.params.id;
 
-//callbacks
+  const results = students.map((student) => {
+    if (student.Id === id) {
+      return { ...student, ...update };
+    }
 
+    return student;
+  });
+  // console.log(students.name);
 
-//middlewares
-app.use(bodyParser.json({extended:false}));
+  res.send(results);
+
+  // const id =req.params.Id;
+
+  // if (students.Id == id) {
+  //   students = { ...students, ...req.body };
+  //   res.json;
+  // }
+
+  // students.forEach((studt)=>{
+  //   console.log(studt.name)
+  // })
+};
+
+//Middlewares
+app.use(bodyParser.json({ extended: false }));
 // app.use(bodyParser)
 
 //routes
-app.get("/students", StudentsGroup)
-app.get("/students/:id", studentId)
-app.post('/students/create-student', createStudent)
-app.delete("/students/delstudent/:id", deleteStudents)
-app.get("/students/update-student/", updateStudents)
-
+app.get("/students", StudentsGroup);
+app.get("/students/:id", studentId);
+app.post("/students/create-student", createStudent);
+app.delete("/students/delstudent/:id", deleteStudents);
+app.put("/students/update-student/:id", updateStudents);
 
 //listening port
-
-app.listen(5000)
+const PORT = 5000;
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
